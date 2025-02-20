9509e31f78bab3345bf8d462db44a5fb32a09fb6
JellkaGamez <jellkagamez14@gmail.com>
2025-02-20 16:59:08 -0500

    fixed decompression bug

--- app.js
+++ app.js
@@ -2,8 +2,31 @@ const fs = require("graceful-fs");
 const path = require("node:path");
 const convert = require("gd-level-to-json");
 
-// scan the levels folder
+// text after
+function after(text, after) {
+    return text.substring(text.indexOf(after) + after.length);
+}
+
+// text before
+function before(text, before) {
+    return text.substring(0, text.indexOf(before));
+}
+
+// take raw level data from levels folder and put it in levels_data folder
 fs.readdirSync(path.join(__dirname, "levels")).forEach((file) => {
+    if (file.endsWith(".gmd")) {
+        const name = file.replace(".gmd", "");
+        // read file data
+        let level = fs.readFileSync(path.join(__dirname, "levels", file));
+        level = after(level, "<s>");
+        level = after(level, "<s>");
+        level = before(level, "</s>");
+        fs.writeFileSync(path.join(__dirname, "levels_data", name + ".json"), level.toString());
+    }
+});
+
+// scan the levels_data folder
+fs.readdirSync(path.join(__dirname, "levels_data")).forEach((file) => {
     if (file.endsWith(".gmd")) {
         const name = file.replace(".gmd", "");
         const level = convert(path.join(__dirname, "levels", file));
