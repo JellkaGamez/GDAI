6800c46c2b0d808f5c237efa1b2e9cd034db938a
JellkaGamez <jellkagamez14@gmail.com>
2025-02-20 16:55:54 -0500

    started doing some basic coding

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
+const fs = require("graceful-fs");
+const path = require("node:path");
 const convert = require("gd-level-to-json");
+
+console.log(convert(fs.readFileSync(path.resolve(__dirname, "level.txt")).toString()));
